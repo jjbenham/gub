0637e2fe3b66da520185684863d5eea962f0d8ea
Jeremiah Benham <jjbenham@chicagoguitar.com>
2013-01-22 09:18:13 -0600

    added portmidi.py to the specs. It only compiles for linux at the moment though

---
+++ gub/specs/portmidi.py
@@ -0,0 +1,13 @@
+from gub import target
+from gub import tools
+
+class Portmidi (target.AutoBuild):
+  source = 'http://www.denemo.org/downloads/gub/libportmidi-0.217.tar.gz'
+  
+  #dependencies = ['']
+
+  #patches = ['evince_stripped.patch']
+  #configure_flags = (tools.AutoBuild.configure_flags
+#			   + ' --without-gtk-unix-print')
+ 
+               
