efd798bf26f0e36b15b94bf02e1c77614b47a300
Jeremiah Benham <jjbenham@chicagoguitar.com>
2013-01-22 11:32:31 -0600

    added portmidi in the specs

--- gub/specs/portmidi.py
+++ gub/specs/portmidi.py
@@ -3,11 +3,12 @@ from gub import tools
 
 class Portmidi (target.AutoBuild):
   source = 'http://www.denemo.org/downloads/gub/libportmidi-0.217.tar.gz'
+
+class Portmidi__mingw (target.AutoBuild):
+  source = 'http://www.denemo.org/downloads/gub/libportmidi-0.218.tar.gz'
   
   #dependencies = ['']
 
   #patches = ['evince_stripped.patch']
   #configure_flags = (tools.AutoBuild.configure_flags
-#			   + ' --without-gtk-unix-print')
- 
-               
+#
