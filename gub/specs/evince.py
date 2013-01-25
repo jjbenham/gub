1e09eed803a201952adfcd89ddb41b4f7b20391e
Jeremiah Benham <jjbenham@chicagoguitar.com>
2013-01-24 22:40:26 -0600

    I created my own tarball of a stripped down evince

--- gub/specs/evince.py
+++ gub/specs/evince.py
@@ -2,13 +2,13 @@ from gub import target
 from gub import tools
 
 class Evince (target.AutoBuild):
-  source = 'http://ftp.acc.umu.se/pub/GNOME/sources/evince/2.30/evince-2.30.3.tar.bz2'
+  source = 'http://www.denemo.org/downloads/gub/evince-2.30.3.tar.bz2'
   
   dependencies = ['intltool',
 #		  'libxml2-devel',
 		  'poppler-devel']
 
-  patches = ['evince_stripped.patch']
+  #patches = ['evince_stripped.patch']
   configure_flags = (tools.AutoBuild.configure_flags
                            + ' --without-help'
 			   + ' --without-libgnome'
@@ -24,24 +24,11 @@ class Evince (target.AutoBuild):
 			   + ' --disable-previewer'
 			   + ' --disable-nls'
 			   + ' --disable-scrollkeeper'
-			   + ' --without-gtk-unix-print')
-
-  def compile (self):
-  	self.system ('cd %(builddir)s/libdocument && make')
-        self.system ('cd %(builddir)s/libview && make')
-  	self.system ('cd %(builddir)s/backend && make')
-  def install (self):
-#        self.system ('cd %(builddir)s/libdocument && make DESTDIR=%(install_root)s install')
-#        self.system ('cd %(builddir)s/libview && make DESTDIR=%(install_root)s install')
-#	self.system ('cd %(builddir)s/backend && make DESTDIR=%(install_root)s install')
-	self.system ('cd %(builddir)s/ && make DESTDIR=%(install_root)s/ install')
-
-#        self.system ('cd %(builddir)s/ && cp -pv evince-document-2.30.pc %(install_root)s/usr/lib/pkgconfig')
-#        self.system ('cd %(builddir)s/ && cp -pv evince-view-2.30.pc %(install_prefix)s/lib/pkgconfig')
-#	self.system ('install -m755 %(builddir)s/evince-view-2.30.pc  %(install_prefix)s/lib/pkgconfig')
-
-#	also need evince-view.h and evince-document.h to be installed
-#gub/specs/zlib.py:        self.system ('cd %(srcdir)s/contrib/minizip && cp ioapi.h iowin32.h mztools.h unzip.h zip.h %(install_prefix)s/include')
+			   + ' --without-gtk-unix-print'
+			   + ' --disable-tiff'
+			   + ' --disable-comics'
+			   + ' --disable-tests'	
+			   + ' --disable-dvi')
 
 #evince-document-2.30.pc target/darwin-x86/root/usr/lib/pkgconfig/x11.pc
 #evince-view-2.30.pc
