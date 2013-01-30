81d0cdb87bee19c0a1456459403dc025b56d03a1
Jeremiah Benham <jjbenham@chicagoguitar.com>
2013-01-29 23:57:55 -0600

    add the portmidi file so that we don't have to include it

---
+++ gub/specs/portmidi.py
@@ -0,0 +1,14 @@
+from gub import target
+from gub import tools
+
+class Portmidi (target.AutoBuild):
+  source = 'http://www.denemo.org/downloads/gub/libportmidi-0.217.tar.gz'
+
+class Portmidi__mingw (target.AutoBuild):
+  source = 'http://www.denemo.org/downloads/gub/libportmidi-0.218.tar.gz'
+  
+  #dependencies = ['']
+
+  #patches = ['evince_stripped.patch']
+  #configure_flags = (tools.AutoBuild.configure_flags
+#
