2af6e10e8acc0d26c10c8f44fecc33299201f51d
Jeremiah Benham <jjbenham@chicagoguitar.com>
2013-03-12 16:37:57 -0500

    this is a work in progress. It buids but can't be linked against

--- gub/specs/portmidi.py
+++ gub/specs/portmidi.py
@@ -6,9 +6,6 @@ class Portmidi (target.AutoBuild):
 
 class Portmidi__mingw (target.AutoBuild):
   source = 'http://www.denemo.org/downloads/gub/libportmidi-0.218.tar.gz'
-  
-  #dependencies = ['']
+class Portmidi__darwin (target.AutoBuild):
+  source = 'http://www.denemo.org/downloads/gub/libportmidi-0.216.1.tar.gz'
 
-  #patches = ['evince_stripped.patch']
-  #configure_flags = (tools.AutoBuild.configure_flags
-#
