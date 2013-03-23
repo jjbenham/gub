4557481649a3f37db2968f1ce2cf20bd77df9ad1
Jeremiah Benham <jjbenham@chicagoguitar.com>
2013-03-23 06:46:49 -0500

    Lets not turn off png. We might want that

--- gub/specs/gdk-pixbuf-2.py
+++ gub/specs/gdk-pixbuf-2.py
@@ -2,14 +2,14 @@ from gub import gnome
 from gub import target
 
 class Gdk_pixbuf_2 (target.AutoBuild):
-    source = 'http://ftp.gnome.org/pub/GNOME/platform/2.32/2.32.1/sources/gdk-pixbuf-2.22.1.tar.gz'
+    source = 'http://ftp.gnome.org/pub/GNOME/sources/gdk-pixbuf/2.24/gdk-pixbuf-2.24.0.tar.bz2'
     dependencies = ['tools::libtool', 'tools::glib', 'libtiff-devel', 'libpng-devel', 'libjpeg-devel']
-    #patches = ['gdk-pixbuf-mmx.patch']
+    patches = ['gdk-pixbuf-2.24-gio-sniff.patch']
     configure_flags = (target.AutoBuild.configure_flags
 			   + ' --disable-glibtest'
 			   + ' --enable-introspection=no'
 			   + ' --disable-introspection'
-			   + ' --without-libpng'
+			   #+ ' --without-libpng'
 			   + ' --enable-gdiplus'
 			   + ' --disable-modules'
 			   + ' --disable-dependency-tracking'
