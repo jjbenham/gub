7da9d7f9178457d1254040fa5e5ebfb3f8de81e9
Jeremiah Benham <jjbenham@chicagoguitar.com>
2013-03-23 06:47:17 -0500

    use denemo 1.0

--- gub/specs/denemo.py
+++ gub/specs/denemo.py
@@ -114,7 +114,7 @@ install -m755 %(builddir)s/src/denemo-console.exe %(install_prefix)s/bin/denemo-
 Denemo__mingw = Denemo__mingw__console
 
 class Denemo__darwin (Denemo):
-    source = 'http://www.denemo.org/downloads/gub/denemo-1.0.0~rc11.tar.gz'
+    source = 'http://www.denemo.org/downloads/gub/denemo-1.0.0.tar.gz'
     #source = 'git://git.savannah.gnu.org/denemo.git'
     #branch = 'darwin'
     patches = ['denemo.main.c-envelope.patch', 'denemo.utils.c-debug.patch']
