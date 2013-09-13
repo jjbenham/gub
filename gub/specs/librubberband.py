cb4ad55bd3a4e01a8ccb21615679a160b1b6c577
Jeremiah Benham <jjbenham@chicagoguitar.com>
2013-09-12 22:20:46 -0500

    add vamp plugin and librubberband spec files

---
+++ gub/specs/librubberband.py
@@ -0,0 +1,10 @@
+from gub import target
+
+class Librubberband (target.AutoBuild):
+    source = 'http://code.breakfastquay.com/attachments/download/34/rubberband-1.8.1.tar.gz'
+    dependencies = ['vamp-plugin-sdk', 'libsamplerate', 'libsndfile', 'libfftw']
+    configure_variables = (target.AutoBuild.configure_variables
+ 			   + ' Vamp_CFLAGS="-I%(system_prefix)s/../vamp-sdk/" '
+			   + ' Vamp_LIBS="-L%(system_prefix)s/../vamp-plugin-sdk-2.5-binaries-win32-mingw -lvamp-sdk" ')
+#    patches = ['librubberband_rubberband-c.h.patch']
+
