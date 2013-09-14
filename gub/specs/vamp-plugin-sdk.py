b3bb70f39b5314c2ecd86e149405bd81a2d0eb4a
Jeremiah Benham <jjbenham@chicagoguitar.com>
2013-09-14 00:15:07 -0500

    import changes from linux branch

--- gub/specs/vamp-plugin-sdk.py
+++ gub/specs/vamp-plugin-sdk.py
@@ -1,13 +1,7 @@
 from gub import target
 from gub import build
 
-class Vamp_plugin_sdk (build.BinaryBuild, build.SdkBuild):
-    source = 'http://code.soundsoftware.ac.uk/attachments/download/694/vamp-plugin-sdk-2.5-binaries-win32-mingw.zip'
-    #source = 'http://code.soundsoftware.ac.uk/attachments/download/690/vamp-plugin-sdk-2.5.tar.gz'
-    dependencies = ['vamp-dev']
-    def untar (self):
-        build.BinaryBuild.untar (self)
-#        self.system ('''
-#cd  %(srcdir)s/vamp-plugin-sdk-2.4-binaries-win32-mingw && mkdir usr && mv *.dll *.a usr/
-#''')
+class Vamp_plugin_sdk (target.AutoBuild):
+    source = 'http://code.soundsoftware.ac.uk/attachments/download/690/vamp-plugin-sdk-2.5.tar.gz'
+    dependencies = ['libsndfile']
 
