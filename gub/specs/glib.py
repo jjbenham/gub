a683a0681f2f562cf03d5eed1acca6146c3c09a9
Jeremiah Benham <jjbenham@chicagoguitar.com>
2013-12-12 11:08:20 -0600

    darwin needs this patch to compile glib

--- gub/specs/glib.py
+++ gub/specs/glib.py
@@ -39,7 +39,7 @@ class Glib__darwin (Glib):
         Glib.configure (self)
         self.file_sub ([('nmedit', '%(target_architecture)s-nmedit')],
                        '%(builddir)s/libtool')
-
+    patches = ['patches/glib-2.27.ZLIB_VERNUM.patch']
 class Glib__darwin__x86 (Glib__darwin):
     # LIBS bugfix from:
     #   https://bugzilla.gnome.org/show_bug.cgi?id=586150
