f746b583b5c6431c5be45905483aaa158481776d
Jeremiah Benham <jjbenham@chicagoguitar.com>
2013-12-13 22:05:34 -0600

    darwin needs a specific hacked version of portmid. Maybe if the tools were updated. Maybe then this would compile normally

--- gub/specs/portmidi.py
+++ gub/specs/portmidi.py
@@ -6,3 +6,7 @@ class Portmidi (target.AutoBuild):
 
 class Portmidi__mingw (target.AutoBuild):
   source = 'http://www.denemo.org/downloads/gub/libportmidi-0.218.tar.gz'
+
+class Portmidi__darwin (target.AutoBuild):
+  source = 'http://www.denemo.org/downloads/gub/libportmidi-0.216.1.tar.gz'
+
