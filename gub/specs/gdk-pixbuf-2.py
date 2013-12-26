1dfe32e0fc2aeedcbc2ce0ab0701888d68eebac8
Jeremiah Benham <jjbenham@chicagoguitar.com>
2013-12-25 20:04:13 -0500

    mingw and darwin need differenent version of gdk-pixbuf-2

--- gub/specs/gdk-pixbuf-2.py
+++ gub/specs/gdk-pixbuf-2.py
@@ -2,7 +2,34 @@ from gub import gnome
 from gub import target
 
 class Gdk_pixbuf_2 (target.AutoBuild):
+#    source = 'http://ftp.gnome.org/pub/GNOME/platform/2.32/2.32.1/sources/gdk-pixbuf-2.22.1.tar.gz'
+#    patches = ['gdk-pixbuf-mmx.patch']
+    source = 'http://ftp.gnome.org/pub/GNOME/sources/gdk-pixbuf/2.24/gdk-pixbuf-2.24.0.tar.bz2'
+    dependencies = ['tools::libtool', 'tools::glib', 'libtiff-devel', 'libpng', 'libjpeg-devel']
+    patches = ['gdk-pixbuf-mmx.patch', 'gdk-pixbuf-2.24-gio-sniff.patch']
+
+    configure_flags = (target.AutoBuild.configure_flags
+			   + ' --disable-glibtest'
+			   + ' --enable-introspection=no'
+			   + ' --disable-introspection'
+			   + ' --enable-gdiplus'
+			   + ' --disable-modules'
+			   + ' --disable-dependency-tracking'
+				)
+class Gdk_pixbuf_2__mingw(target.AutoBuild):
     source = 'http://ftp.gnome.org/pub/GNOME/platform/2.32/2.32.1/sources/gdk-pixbuf-2.22.1.tar.gz'
-    dependencies = ['tools::libtool', 'glib-devel', 'libtiff-devel', 'libpng-devel', 'libjpeg-devel']
+#    source = 'http://ftp.gnome.org/pub/GNOME/sources/gdk-pixbuf/2.24/gdk-pixbuf-2.24.0.tar.bz2'
+    dependencies = ['tools::libtool', 'tools::glib', 'libtiff-devel', 'libpng', 'libjpeg-devel']
     patches = ['gdk-pixbuf-mmx.patch']
+
+    configure_flags = (target.AutoBuild.configure_flags
+			   + ' --disable-glibtest'
+			   + ' --enable-introspection=no'
+			   + ' --disable-introspection'
+			   + ' --disable-gdiplus'
+			   + ' --disable-modules'
+			   #+ ' --without-libtiff'
+			   #+ ' --without-libjpeg'
+			   + ' --disable-dependency-tracking'
+				)
 
