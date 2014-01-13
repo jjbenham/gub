fe678d7b8c05cdf5a1de564a0700b8ddd72dff2c
Jeremiah Benham <jjbenham@chicagoguitar.com>
2014-01-13 17:20:51 -0500

    make sure to install intltool as a tool

--- gub/specs/intltool.py
+++ gub/specs/intltool.py
@@ -4,4 +4,15 @@ class Intltool (tools.AutoBuild):
     source = 'ftp://ftp.gnome.org/pub/GNOME/sources/intltool/0.40/intltool-0.40.5.tar.gz'
     dependencies = [
             'tools::perl-xml-parser',
+	    'tools::intltool'
             ]
+class Intltool__tools (tools.AutoBuild):
+    source = 'ftp://ftp.gnome.org/pub/GNOME/sources/intltool/0.40/intltool-0.40.5.tar.gz'
+    dependencies = [
+           'tools::perl-xml-parser'
+			]            
+    configure_flags = (tools.AutoBuild.configure_flags
+                       + ' --build=%(build_architecture)s'
+                       + ' --host=%(build_architecture)s'
+                       + ' --target=%(build_architecture)s'
+                       )
