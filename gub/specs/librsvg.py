814365f8709ae4e9648a4bbc7bf1789abcf47240
Jeremiah Benham <jjbenham@chicagoguitar.com>
2014-01-24 12:11:38 -0500

    removed some a dependencies

--- gub/specs/librsvg.py
+++ gub/specs/librsvg.py
@@ -3,15 +3,12 @@ from gub import target
 class Librsvg (target.AutoBuild):
     source = 'http://ftp.gnome.org/pub/GNOME/sources/librsvg/2.26/librsvg-2.26.0.tar.gz'
     dependencies = ['tools::libtool',
-                'cairo-devel',
                 'fontconfig-devel',
                 'glib-devel',
                 'gtk+-devel',
-                'pangocairo-devel',
                 'libxml2-devel']
+    configure_flags = (target.AutoBuild.configure_flags
+                       	+ ' --enable-static'
+			+ ' --disable-shared'
+                       	)
 
-class Librsvg__darwin (Librsvg):
-    dependencies = [x for x in Librsvg.dependencies
-                if x.replace ('-devel', '') not in [
-                'libxml2', # Included in darwin-sdk, hmm?
-                ]]
