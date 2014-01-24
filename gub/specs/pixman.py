bd21f2b0407fa82aed71a64e8b17d1e28f7e380f
Jeremiah Benham <jjbenham@chicagoguitar.com>
2014-01-24 12:17:42 -0500

    pixman seems to depend on libpng

--- gub/specs/pixman.py
+++ gub/specs/pixman.py
@@ -2,7 +2,7 @@ from gub import target
 
 class Pixman (target.AutoBuild):
     source = 'http://xorg.freedesktop.org/archive/individual/lib/pixman-0.24.0.tar.gz'
-    dependencies = ['libtool']
+    dependencies = ['libtool', 'libpng']
 
 class Pixman__linux__ppc (Pixman):
     patches = ['pixman-0.13.2-auxvec.patch']
