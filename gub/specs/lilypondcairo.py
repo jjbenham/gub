e555688de92a7ea85941e990b2119a812193cb7c
Jeremiah Benham <jjbenham@chicagoguitar.com>
2014-01-24 12:12:47 -0500

    removed some dependencies and unused patch

--- gub/specs/lilypondcairo.py
+++ gub/specs/lilypondcairo.py
@@ -7,33 +7,19 @@ from gub.specs import lilypond
 
 class Lilypondcairo (lilypond.Lilypond):
     source = 'http://download.linuxaudio.org/lilypond/sources/v2.18/lilypond-2.18.0.tar.gz'
-    dependencies = [x.replace ('pango', 'pangocairo')
-                    for x in lilypond.Lilypond.dependencies]
     def get_conflict_dict (self):
         return {'': ['lilypond']}
 
 class Lilypondcairo__mingw (lilypond.Lilypond__mingw):
     source = Lilypondcairo.source
-    dependencies = [x.replace ('pango', 'pangocairo')
-                for x in lilypond.Lilypond__mingw.dependencies]
-    #patches = [
-    #    '0003-Start-OTF-font-from-E800-avoids-hardcoded-linux-unic.patch',
-    #    ]
     def get_conflict_dict (self):
         return {'': ['lilypond']}
 
 class Lilypondcairo__darwin (lilypond.Lilypond__darwin):
     source = Lilypondcairo.source
-    dependencies = [x.replace ('pango', 'pangocairo')
-                for x in lilypond.Lilypond__darwin
-                .dependencies]
     def get_conflict_dict (self):
         return {'': ['lilypond']}
 
 class Lilypondcairo__darwin__ppc (lilypond.Lilypond__darwin__ppc):
-    source = Lilypondcairo.source
-    dependencies = [x.replace ('pango', 'pangocairo')
-                for x in lilypond.Lilypond__darwin__ppc
-                .dependencies]
     def get_conflict_dict (self):
         return {'': ['lilypond']}
