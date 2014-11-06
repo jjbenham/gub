6c788b94db299bb942472375e5adb894f57e88aa
Jeremiah Benham <jjbenham@chicagoguitar.com>
2014-11-06 18:56:03 -0500

    use my hacked version of evince

--- gub/specs/evince2.py
+++ gub/specs/evince2.py
@@ -2,33 +2,53 @@ from gub import target
 from gub import tools
 
 class Evince2 (target.AutoBuild):
-  source = 'http://ftp.gnome.org/pub/GNOME/sources/evince/2.32/evince-2.32.0.tar.bz2'
+  source = 'http://www.denemo.org/downloads/gub/evince-2.30.3.tar.bz2'
   
-  dependencies = [
-		  #'intltool',
-		  'tools::intltool',
+  dependencies = ['intltool',
+#		  'libxml2-devel',
 		  'gtk2',
-		  'poppler-devel']
-  patches = ['evince-4-Makefile.patch']
+		  'poppler']
+
+#  patches = ['evince_stripped.patch']
   configure_flags = (tools.AutoBuild.configure_flags
-			   #+ ' --with-libintl-prefix=%(install_prefix)s'
-                           #+ ' --enable-static'
+                           + ' --without-help'
 			   + ' --without-libgnome'
 			   + ' --without-gconf'
                            + ' --without-keyring'
-                           + ' --with-platform=win32'
-			   + ' --with-smclient-backend=win32' #not sure what this is
+                           + ' --with-platform=gnome'
+			   + ' --with-smclient-backend=no'
 			   + ' --disable-help'
 			   + ' --disable-thumbnailer'
 			   + ' --disable-nautilus'
 			   + ' --disable-dbus'
 			   + ' --disable-gtk-doc'
-			   #+ ' --disable-pdf' #FIXME probably need pdf support
-			   + ' --disable-previewer' #not sure if this is needed
+			   + ' --disable-previewer'
 			   + ' --disable-nls'
+			   + ' --disable-scrollkeeper'
+			   + ' --disable-tiff'
+			   + ' --disable-comics'
 			   + ' --without-gtk-unix-print')
 
+class Evince2__darwin__x86 (Evince2):
   configure_variables = (tools.AutoBuild.configure_variables
+                           + ' CFLAGS="-g -O0" ')
+
+class Evince2__linux__x86 (Evince2):
+  configure_variables = (tools.AutoBuild.configure_variables
+                           + ' CFLAGS="-g -O0 -DHAVE_POPPLER_PAGE_RENDER" ')
+
+class Evince2__mingw (Evince2):
+#  patches = ['evince-icon.patch']
+  configure_flags = (Evince2.configure_flags
+                           + ' --with-platform=win32'
+			   + ' --with-smclient-backend=win32')
+
+  configure_variables = (Evince2.configure_variables
                            + ' CPPFLAGS="-D_WIN32_WINNT=0x0501"')
+class Evince2__linux__x86 (Evince2):
+  configure_flags = (Evince2.configure_flags
+                           + ' --with-platform=gnome'
+			   + ' --with-smclient-backend=xsmp')
+  dependencies = ['libsm']
 
-
+               
