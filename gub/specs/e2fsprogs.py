528c0200d3669a7f05f522cd00356486aa9e2bf0
Jeremiah Benham <jjbenham@chicagoguitar.com>
2014-11-06 18:55:34 -0500

    broken link

--- gub/specs/e2fsprogs.py
+++ gub/specs/e2fsprogs.py
@@ -3,7 +3,7 @@ from gub import target
 # FIXME: updateme and dependants, package has finally been split
 # http://sourceforge.net/projects/e2fsprogs/files/
 class E2fsprogs (target.AutoBuild):
-    source = 'http://surfnet.dl.sourceforge.net/sourceforge/e2fsprogs/e2fsprogs-1.38.tar.gz'
+    source = 'http://pkgs.fedoraproject.org/repo/pkgs/e2fsprogs/e2fsprogs-1.38.tar.gz/d774d4412bfb80d12cf3a4fdfd59de5a/e2fsprogs-1.38.tar.gz'
     configure_flags = (target.AutoBuild.configure_flags
                        + '  --enable-elf-shlibs')
     install_command = (target.AutoBuild.install_command
