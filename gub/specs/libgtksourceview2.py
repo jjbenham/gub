1d68a3023e407fc6060096d8be4555633da5f554
Jeremiah Benham <jjbenham@chicagoguitar.com>
2014-11-06 19:00:56 -0500

    downgrade gtksourceview. I don't remember why this was necessary

--- gub/specs/libgtksourceview2.py
+++ gub/specs/libgtksourceview2.py
@@ -1,7 +1,8 @@
 from gub import target
 
 class Libgtksourceview2 (target.AutoBuild):
-    source = 'http://ftp.gnome.org/pub/gnome/sources/gtksourceview/2.11/gtksourceview-2.11.2.tar.gz'
+    source = 'http://ftp.gnome.org/pub/gnome/sources/gtksourceview/2.6/gtksourceview-2.6.2.tar.gz'
     dependencies = [
+	'tools::intltool',
         'gtk2',
         ]
