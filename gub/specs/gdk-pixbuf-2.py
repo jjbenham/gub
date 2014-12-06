c1248a5fc3df3827fa0de2eeb708a01689aab86c
Jeremiah Benham <jjbenham@chicagoguitar.com>
2014-12-05 22:21:16 -0500

    upgrade gdk-pixbuf-2

--- gub/specs/gdk-pixbuf-2.py
+++ gub/specs/gdk-pixbuf-2.py
@@ -4,9 +4,9 @@ from gub import target
 class Gdk_pixbuf_2 (target.AutoBuild):
 #    source = 'http://ftp.gnome.org/pub/GNOME/platform/2.32/2.32.1/sources/gdk-pixbuf-2.22.1.tar.gz'
 #    patches = ['gdk-pixbuf-mmx.patch']
-    source = 'http://ftp.gnome.org/pub/GNOME/sources/gdk-pixbuf/2.24/gdk-pixbuf-2.24.1.tar.xz'
+    source = 'http://ftp.gnome.org/pub/GNOME/sources/gdk-pixbuf/2.26/gdk-pixbuf-2.26.5.tar.xz'
     dependencies = ['tools::libtool', 'tools::glib', 'libtiff-devel', 'libpng', 'libjpeg-devel']
-    patches = ['gdk-pixbuf-2.24-gio-sniff.patch']
+    patches = ['gdk-pixbuf-2.28.2-no-gio-sniff.patch']
 
     configure_flags = (target.AutoBuild.configure_flags
 			   + ' --disable-glibtest'
