522febdf6d88815ae98a11a71ad663119d8f9adf
Jeremiah Benham <jjbenham@chicagoguitar.com>
2014-12-05 22:14:02 -0500

    upgrade xproto

--- gub/specs/xproto.py
+++ gub/specs/xproto.py
@@ -1,5 +1,5 @@
 from gub import target
 
 class Xproto (target.AutoBuild):
-    source = 'http://xorg.freedesktop.org/releases/X11R7.4/src/proto/xproto-7.0.13.tar.gz'
+    source = 'http://www.x.org/releases/X11R7.7/src/everything/xproto-7.0.23.tar.bz2'
     dependencies = ['tools::libtool']
