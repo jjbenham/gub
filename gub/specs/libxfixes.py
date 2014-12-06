1c487b14aa28c8da933d69d736556a451d4722dc
Jeremiah Benham <jjbenham@chicagoguitar.com>
2014-12-05 22:17:38 -0500

    upgrade libxfixes

--- gub/specs/libxfixes.py
+++ gub/specs/libxfixes.py
@@ -2,4 +2,4 @@ from gub import target
 
 class Libxfixes (target.AutoBuild):
     source = 'http://xorg.freedesktop.org/releases/X11R7.4/src/everything/libXfixes-4.0.3.tar.gz'
-    dependencies = ['tools::libtool', 'fixesproto-devel', 'libx11-devel', 'libxau-devel', 'libxdmcp-devel']
+    dependencies = ['tools::libtool', 'fixesproto-devel', 'libx11-devel', 'libxau-devel', 'libxdmcp-devel', 'libxext']
