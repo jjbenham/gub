f356fc33bb02416ffe33c9cad19d51503ba7a6b2
Jeremiah Benham <jjbenham@chicagoguitar.com>
2014-12-05 22:12:44 -0500

    upgrade xectproto

--- gub/specs/xextproto.py
+++ gub/specs/xextproto.py
@@ -1,5 +1,5 @@
 from gub import target
 
 class Xextproto (target.AutoBuild):
-    source = 'http://xorg.freedesktop.org/releases/X11R7.4/src/proto/xextproto-7.0.3.tar.gz'
+    source = 'http://www.x.org/releases/X11R7.7/src/everything/xextproto-7.2.1.tar.bz2'
     dependencies = ['tools::libtool']
