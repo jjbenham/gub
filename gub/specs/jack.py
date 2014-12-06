842fd85909a762180042e295f227b573132edf95
Jeremiah Benham <jjbenham@chicagoguitar.com>
2014-12-05 22:18:18 -0500

    upgrade jack

--- gub/specs/jack.py
+++ gub/specs/jack.py
@@ -1,6 +1,7 @@
 from gub import target
 
 class Jack (target.WafBuild):
-    source = 'svn+http://subversion.jackaudio.org/jack/trunk/jack'
-    source = 'http://www.grame.fr/~letz/jack-1.9.2.tar.bz2'
-    dependencies = ['tools::automake', 'tools::pkg-config',]
+    source = 'https://dl.dropboxusercontent.com/u/28869550/jack-1.9.10.tar.bz2'
+    patches = ['jack-1.9.10.ppoll.patch']
+    dependencies = ['tools::automake', 'tools::pkg-config','dbus', 'portaudio', 'alsa']
+ 
