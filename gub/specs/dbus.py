a3a48cc432a985c59240d41389836d2f40b7780a
Jeremiah Benham <jjbenham@chicagoguitar.com>
2014-12-05 22:09:05 -0500

    upgrade dbus

--- gub/specs/dbus.py
+++ gub/specs/dbus.py
@@ -1,9 +1,13 @@
 from gub import target
 
 class Dbus (target.AutoBuild):
-    source = 'http://dbus.freedesktop.org/releases/dbus/dbus-1.2.14.tar.gz'
+    source = 'http://dbus.freedesktop.org/releases/dbus/dbus-1.8.2.tar.gz'
     dependencies = ['tools::automake', 'tools::pkg-config',
                 ]
     config_cache_overrides = target.AutoBuild.config_cache_overrides + '''
 ac_cv_have_abstract_sockets=${ac_cv_have_abstract_sockets=yes}
 '''
+    configure_flags = (target.AutoBuild.configure_flags
+                       + '  --with-x=yes'
+		       + '  --disable-inotify'
+                       )
